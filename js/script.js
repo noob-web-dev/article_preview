604299a48bfc210c05b7849f690a98b9fee0427d
Ravi <ravi.9910@yahoo.com>
2023-09-27 14:49:55 +0530

    little refactoring in js

--- js/script.js
+++ js/script.js
@@ -4,41 +4,8 @@ const authorProfileElement = document.getElementById("authorProfile");
 const popupMenuElement = document.getElementById("popupMenu");
 
 popupToggleButton.addEventListener("click", (e) => {
-  toggleBtn();
-  toggleLinksContainer();
-  toggleAuthorProfile();
-  togglePopupMenu();
+  popupToggleButton.classList.toggle("btn__popup--active");
+  linksContainerElement.classList.toggle("article__linksContainer--active");
+  authorProfileElement.classList.toggle("article__authorProfile--hide");
+  popupMenuElement.classList.toggle("popup__menu--hide");
 });
-
-const toggleBtn = () => {
-  if (popupToggleButton.classList.contains("btn__popup--active")) {
-    popupToggleButton.classList.remove("btn__popup--active");
-  } else {
-    popupToggleButton.classList.add("btn__popup--active");
-  }
-};
-
-const toggleLinksContainer = () => {
-  if (
-    linksContainerElement.classList.contains("article__linksContainer--active")
-  ) {
-    linksContainerElement.classList.remove("article__linksContainer--active");
-  } else {
-    linksContainerElement.classList.add("article__linksContainer--active");
-  }
-};
-
-const toggleAuthorProfile = () => {
-  if (authorProfileElement.classList.contains("article__authorProfile--hide")) {
-    authorProfileElement.classList.remove("article__authorProfile--hide");
-  } else {
-    authorProfileElement.classList.add("article__authorProfile--hide");
-  }
-};
-const togglePopupMenu = () => {
-  if (popupMenuElement.classList.contains("popup__menu--hide")) {
-    popupMenuElement.classList.remove("popup__menu--hide");
-  } else {
-    popupMenuElement.classList.add("popup__menu--hide");
-  }
-};
